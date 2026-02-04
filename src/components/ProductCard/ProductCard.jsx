84fe230a0b3312f6d4181013a9c1362b27117dc4
Ikram Boukhelif <bk-ikram@hotmail.com>
2026-02-04 18:23:17 +0000

    This is a test of codesandbox git push. No real changes made.

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,25 +1,28 @@
-import { useState } from 'react';
+import { useState } from "react";
 import styles from "./ProductCard.module.css";
 
-export default function ProductCard(props){
-
-    return (
-        <div className={styles.productCard}>
-            <div className={styles.imgContainer}>
-                <img className={styles.productImage} src={props.image} alt={props.title} />
-            </div>
-            <div className={styles.details}>
-                <h2 className={styles.productTitle}>{props.title}</h2>
-                <h3 className={styles.productPrice}>${props.price}</h3>
-                <div className={styles.cardActions}>
-                    <div className={styles.quantityControl}>
-                        <button className={styles.decrease}>-</button>
-                        <input className={styles.quantity} value = {1}/>
-                        <button className={styles.increase}>+</button>
-                    </div>
-                <button className={styles.addToCartBtn}>Add to Cart</button>
-                </div>
-            </div>
+export default function ProductCard(props) {
+  return (
+    <div className={styles.productCard}>
+      <div className={styles.imgContainer}>
+        <img
+          className={styles.productImage}
+          src={props.image}
+          alt={props.title}
+        />
+      </div>
+      <div className={styles.details}>
+        <h2 className={styles.productTitle}>{props.title}</h2>
+        <h3 className={styles.productPrice}>${props.price}</h3>
+        <div className={styles.cardActions}>
+          <div className={styles.quantityControl}>
+            <button className={styles.decrease}>-</button>
+            <input className={styles.quantity} value={1} />
+            <button className={styles.increase}>+</button>
+          </div>
+          <button className={styles.addToCartBtn}>Add to Cart</button>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+} //testing codesandbox git features
